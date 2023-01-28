843c3aef8d9c22562472dfa6b6d0d51beb39e65d
Mark Rywell Gaje <gopro855@gmail.com>
2023-01-28 23:19:17 +0800

    Added Weather Station OLED to display data on OLED

---
+++ WeatherStation_OLED.py
@@ -0,0 +1,180 @@
+# GUI Components
+
+from tkinter import *
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+import matplotlib
+
+matplotlib.use('Qt5Agg', force=True)
+
+# IOT Components
+
+import time
+
+import Adafruit_DHT
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import bmpsensor
+
+import smbus
+import time
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None  # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Beaglebone Black pin configuration:
+# RST = 'P9_12'
+# Note the following are only used with SPI:
+# DC = 'P9_15'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# 128x64 display with hardware I2C:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Note you can change the I2C address by passing an i2c_address parameter like:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+
+# Alternatively you can specify an explicit I2C bus number, for example
+# with the 128x32 display you would use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
+# 128x32 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# 128x64 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 😎
+
+# Initialize DHT11
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+
+# Light Sensor
+# Define some constants from the datasheet
+
+DEVICE = 0x23  # Default device I2C address
+
+POWER_DOWN = 0x00  # No active state
+POWER_ON = 0x01  # Power on
+RESET = 0x07  # Reset data register value
+
+# Start measurement at 4lx resolution. Time typically 16ms.
+CONTINUOUS_LOW_RES_MODE = 0x13
+# Start measurement at 1lx resolution. Time typically 120ms
+CONTINUOUS_HIGH_RES_MODE_1 = 0x10
+# Start measurement at 0.5lx resolution. Time typically 120ms
+CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+# Start measurement at 1lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_HIGH_RES_MODE_1 = 0x20
+# Start measurement at 0.5lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_HIGH_RES_MODE_2 = 0x21
+# Start measurement at 1lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_LOW_RES_MODE = 0x23
+
+# bus = smbus.SMBus(0) # Rev 1 Pi uses 0
+bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+
+
+def convertToNumber(data):
+    # Simple function to convert 2 bytes of data
+    # into a decimal number. Optional parameter 'decimals'
+    # will round to specified number of decimal places.
+    result = (data[1] + (256 * data[0])) / 1.2
+    return (result)
+
+
+def readLight(addr=DEVICE):
+    # Read data from I2C interface
+    data = bus.read_i2c_block_data(addr, ONE_TIME_HIGH_RES_MODE_1)
+    return convertToNumber(data)
+
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    humidity_value, temperature_value = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    pressure_value = bmpsensor.readBmp180()
+    lightLevel = readLight()
+
+    if humidity_value is not None and temperature_value is not None and pressure_value is not None:
+
+        i = i + 5
+
+        # temperature_plot.append(temperature_value)
+        # humidity_plot.append(humidity_value)
+        # time_plot.append(i)
+
+        # Write two lines of text.
+
+        draw.text((x, top), "Light: " + format(lightLevel, '.2f') + " lx", font=font, fill=255)
+        draw.text((x, top + 8), "Temperature: " + str(temperature_value) + " C", font=font, fill=255)
+        draw.text((x, top + 16), "Humidity: " + str(humidity_value) + "%", font=font, fill=255)
+        draw.text((x, top + 25), "Pressure: " + str(pressure_value[1]) + " Pa", font=font, fill=255)
+    else:
+        draw.text((x, top), "Weather Station IOT", font=font, fill=255)
+        draw.text((x, top + 16), "Sensor Error", font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(3)
+
+
